eccfb0d0c13431502b3be3dfd309a080b828f058
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-11-27 21:11:59 +0530

    VUE GUI

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    // dark = false
+    breakpoint: {
+      // mobileBreakpoint: 'sm', // This is equivalent to a value of 960
+      // scrollBarWidth: 24
+    },
+  },
+});
---
+++ src/views/Error404.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Error 440</h1>
+</template>
+
+<script>
+export default {
+    name:'Error404',
+    created:function(){
+        // 
+    }
+}
+</script>

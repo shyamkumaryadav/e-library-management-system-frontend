eb1da7d4c43176acfc16befc3df3832b7f866b62
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2021-01-01 22:58:24 +0530

    :v:

--- src/store/modules/Books.js
+++ src/store/modules/Books.js
@@ -10,29 +10,25 @@ export default {
     // Mutat the state (Update state)
     mutations: {
         setBook(state, payload){
-            state.count = payload.books.count
-            state.next = payload.books.next
-            state.previous = payload.books.previous
-            state.results = payload.books.results
+            state.count = payload.data.count
+            state.next = payload.data.next
+            state.previous = payload.data.previous
+            state.results = payload.data.results
             state.isDone = false
         }
 
     },
     // Called from components to commit a mutation (like api call)
     actions: {
-        async getBook({commit}){
-            const book = await fetch(`${process.env.VUE_APP_API_URL}/books`)
-            const json = await book.json();
-            console.log(json);
-            commit('setBook', {
-                books: json
-            })
-
+        getBook({commit}){
+            fetch(`${process.env.VUE_APP_API_URL}/books`)
+            .then(res => res.json())
+            .then(data => commit('setBook', {data}))
         }
     },
     getters:{
-        getBooks(state, payload){
-            return state.BooksList.filter(book => payload.q in book.title) ?? state.BooksList
+        getBooks(state){
+            return state
         }
     },
   }
--- src/views/ViewBook.vue
+++ src/views/ViewBook.vue
@@ -1,28 +1,91 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col cols="12" md="4">
-                <h1 class="pink--text">Views</h1>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col cols="12" md="4">
-                <v-text-field
-                    hide-details
-                    prepend-icon="mdi-magnify"
-                    single-line
-                    v-model="search"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12" md="4" v-for="book in books(search)" :key="book.id">
-                <v-card>
-                    <v-card-title>{{ book.title }}</v-card-title>
-                    <v-card-subtitle>{{ book.id }}</v-card-subtitle>
-                    <v-card-text>{{ book.body }}</v-card-text>
-                </v-card>
+        <p>{{islist}}</p>
+        <v-row v-if="islist">
+           <v-col v-for="book in books.results" :key="book.url" cols="12" lg="3" md="3">
+            <v-card @click="cardUpdate(book.url)">
+                <v-img
+                    :src="book.profile"
+                    class="white--text align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="300px"
+                >
+                    <v-card-title v-text="book.name"></v-card-title>
+                </v-img>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                    <v-icon>mdi-bookmark</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
             </v-col>
         </v-row>  
+        <v-row v-else>
+            <v-col cols="12">
+            <v-card>
+                <v-img
+                    :src="c_id.profile"
+                    class="white--text align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="500px"
+                >   
+                    <v-card-title v-text="c_id.name"></v-card-title>
+                </v-img>
+                <v-card-text>
+                    <v-row
+                        align="center"
+                        class="mx-0"
+                    >
+                        <v-rating
+                        :value="c_id.rating/2"
+                        color="amber"
+                        dense
+                        half-increments
+                        readonly
+                        size="14"
+                        ></v-rating>
+
+                        <div class="grey--text ml-4">
+                        {{c_id.rating}}
+                        </div>
+                    </v-row>
+
+                    <div class="my-4 subtitle-1">
+                        &#8377; {{c_id.cost}}
+                    </div>
+
+                    <div>{{c_id.description}}</div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn icon @click="islist = true">
+                    <v-icon>mdi-window-close</v-icon>
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                    <v-icon>mdi-heart</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                    <v-icon>mdi-bookmark</v-icon>
+                    </v-btn>
+
+                    <v-btn icon>
+                    <v-icon>mdi-share-variant</v-icon>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
@@ -33,15 +96,23 @@ export default {
     data(){
         return {
             search: '',
+            islist: true,
+            c_id:null
         }
+    },
+    methods:{
+        cardUpdate(bookid){
+            this.islist = false
+            this.c_id = this.books.results.filter(val => val.url == bookid)[0]
+        },
     },
     created(){
         this.$store.dispatch('getBook')
     },
     computed:{
-        books(q){
-            return this.$store.getters.getBooks({q})
-        }
+        books(){
+            return this.$store.getters.getBooks
+        },
     }
 }
 </script>
